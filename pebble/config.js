62a9efc0643ceb4994ba3279820c7dd5b83b2d49
Alexander Nilsen <alexander.nilsen.no@gmail.com>
2015-10-08 06:24:34 +0200

    Issue #2 - submit data to emulator or device

--- pebble/config.js
+++ pebble/config.js
@@ -1,3 +1,19 @@
+// Get query variables
+function getQueryParam(variable, defaultValue) {
+  // Find all URL parameters
+  var query = location.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+
+    // If the query variable parameter is found, decode it to use and return it for use
+    if (pair[0] === variable) {
+      return decodeURIComponent(pair[1]);
+    }
+  }
+  return defaultValue || false;
+}
+
 
 $(document).ready(function() {
 
@@ -9,5 +25,8 @@ $(document).ready(function() {
       "mood_step": +$("#mood_step").val()
     };
     console.log(config);
+    // Set the return URL depending on the runtime environment
+    var return_to = getQueryParam('return_to', 'pebblejs://close#');
+    document.location = return_to + encodeURIComponent(JSON.stringify(config));
   });
 });
