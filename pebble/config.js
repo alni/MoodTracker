755fa21a9f872c013a58b5e1685755b363ab7c06
Alexander Nilsen <alexander.nilsen.no@gmail.com>
2015-10-10 06:33:45 +0200

    Issue #2 - add Reminder Days & Hours config
    * Add "reminder_days" input field (min 1 day, max 14 days)
    * Add "reminder_hours" multi-select (from 12am to 11pm)

--- pebble/config.js
+++ pebble/config.js
@@ -18,6 +18,8 @@ function loadConfigData() {
   var $mood_min = $("#mood_min");
   var $mood_max = $("#mood_max");
   var $mood_step = $("#mood_step");
+  var $reminder_days = $("#reminder_days");
+  var $reminder_hours = $("#reminder_hours");
   if (localStorage["mood_min"]) {
     $mood_min.val(localStorage["mood_min"])
   }
@@ -27,23 +29,35 @@ function loadConfigData() {
   if (localStorage["mood_step"]) {
     $mood_step.val(localStorage["mood_step"])
   }
+  if (localStorage["reminder_days"]) {
+    $reminder_days.val(localStorage["reminder_days"]);
+  }
+  if (localStorage["reminder_hours"]) {
+    $reminder_hours.val(localStorage["reminder_hours"].split(","));
+  }
 }
 
 function getConfigData() {
   var $mood_min = $("#mood_min");
   var $mood_max = $("#mood_max");
   var $mood_step = $("#mood_step");
+  var $reminder_days = $("#reminder_days");
+  var $reminder_hours = $("#reminder_hours");
 
   var options = {
     "mood_min": +$mood_min.val(),
     "mood_max": +$mood_max.val(),
-    "mood_step": +$mood_step.val()
+    "mood_step": +$mood_step.val(),
+    "reminder_days": +$reminder_days.val(),
+    "reminder_hours": $reminder_hours.val().map(Number)
   };
 
   // Save for next launch
   localStorage["mood_min"] = options["mood_min"] + "";
   localStorage["mood_max"] = options["mood_max"] + "";
   localStorage["mood_step"] = options["mood_step"] + "";
+  localStorage["reminder_days"] = options["reminder_days"] + "";
+  localStorage["reminder_hours"] = options["reminder_hours"];
 
   console.log("Got options: " + JSON.stringify(options));
   return options;
